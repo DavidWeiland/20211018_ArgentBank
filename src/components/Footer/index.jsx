ae1a62019cd3101077a85b4fbc4ec96d0427ad74
David Weiland <weiland.david@icloud.com>
2021-10-19 12:47:11 +0200

    component Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import '../../utils/Style/main.css'
+
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <p class="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+}
+
+export default Footer;
