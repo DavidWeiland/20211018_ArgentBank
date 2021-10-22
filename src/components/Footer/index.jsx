3ba1f7b4fa6d79029e4a9cb6969e3600d4714135
David Weiland <weiland.david@icloud.com>
2021-10-22 18:19:22 +0200

    modified class to className

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import '../../utils/Style/main.css'
 
 function Footer() {
   return (
-    <footer class="footer">
-      <p class="footer-text">Copyright 2020 Argent Bank</p>
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
     </footer>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,30 +8,30 @@ function Header() {
   const userSigned = true //envoyé par le store
 
   return (
-    <nav class="main-nav">
-      <Link class="main-nav-logo" to="/">
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
         <img
-          class="main-nav-logo-image"
+          className="main-nav-logo-image"
           src={Logo}
           alt="Argent Bank Logo"
         />
-        <h1 class="sr-only">Argent Bank</h1>
+        <h1 className="sr-only">Argent Bank</h1>
       </Link>
       {(userSigned) ? (
         <div>
-        <Link class="main-nav-item" to="/sign-in">
-            <i class="fa fa-user-circle"></i>
+        <Link className="main-nav-item" to="/sign-in">
+            <i className="fa fa-user-circle"></i>
             Sign In
         </Link>
       </div>
       ) : (
         <div>
-        <Link class="main-nav-item" to="/user">
-          <i class="fa fa-user-circle"></i>
+        <Link className="main-nav-item" to="/user">
+          <i className="fa fa-user-circle"></i>
           Tony
         </Link>
-        <Link class="main-nav-item" to="/">
-          <i class="fa fa-sign-out"></i>
+        <Link className="main-nav-item" to="/">
+          <i className="fa fa-sign-out"></i>
           Sign Out
         </Link>
       </div>
