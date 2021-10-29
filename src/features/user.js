4c06046ce6059e54cc3c9867a536f86a6d6cca7b
David Weiland <weiland.david@icloud.com>
2021-10-29 13:33:57 +0200

    organisation store and features

---
+++ src/features/user.js
@@ -0,0 +1,134 @@
+import axios from 'axios'
+import produce from 'immer'
+import { baseUrl } from '../utils/data'
+
+const initialState = {
+  status: 'void',
+  token : null,
+  data: {
+    _id: null,
+    lastName: null,
+    firstName: null,
+    email: null,
+    password: null
+  },
+  error:null
+}
+
+const FETCHING = 'user/fetching'
+const RESOLVED = 'user/resolved'
+const AUTHORIZED = 'user/authorized'
+const REJECTED = 'user/rejected'
+const RESET = 'user/reset'
+
+const userFetching = () => ({ type: FETCHING })
+const userResolved = (data) => ({ type: RESOLVED, payload: data })
+const userAuthorized = (data) => ({ type: AUTHORIZED, payload: data })
+const userRejected = (error) => ({ type: REJECTED, payload: error })
+const userReset = () => ({ type: RESET })
+
+
+export async function createUser(store, body) {
+  const status = store.getState().user.status
+  if (status === 'pending' || status === 'updating') {
+    return
+  }
+  store.dispatch(userFetching())
+  try {
+    const res = await axios.post(`${baseUrl}/user/signup`, body)
+    const data = await res.data.body
+    store.dispatch(userResolved(data))
+  }
+  catch (error) {
+    store.dispatch(userRejected(error))
+  }
+}
+
+export async function authorizeUser(store, body) {
+  const status = store.getState().auth.status
+  if (status === 'pending' || status === 'updating') {
+    return
+  }
+  store.dispatch(userFetching())
+  try {
+    const res = await axios.post(`${baseUrl}/user/login`, body)
+    const data = await res.data.body
+    store.dispatch(userAuthorized(data))
+  }
+  catch (error) {
+    store.dispatch(userRejected(error))
+  }
+}
+
+export async function resetUser(store) {
+    store.dispatch(userReset())
+}
+
+export async function getOrModifyUser(store, method, body) {
+    const status = store.getState().user.status
+    const token = store.getState().user.token
+    const headers = { headers:{"Authorization": `Bearer ${token}`} }
+    console.log(headers)
+    if (status === 'pending' || status === 'updating') {
+      return 
+    }
+    store.dispatch(userFetching())
+    try {
+      const res = await axios({method:method, url:`${baseUrl}/user/profile`, body, headers:headers})
+      const data = await res.data.body
+      store.dispatch(userResolved(data))
+    }
+    catch (error) {
+      store.dispatch(userRejected(error))
+    }
+}
+
+export default function userReducer(state = initialState, action) {
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case FETCHING: {
+        if (draft.status === 'void') {
+          draft.status = 'pending'
+          return
+        }
+        if (draft.status === 'rejected') {
+          draft.error = null
+          draft.status = 'pending'
+          return
+        }
+        if (draft.status === 'resolved' || draft.status === 'authorized') {
+          draft.status = 'updating'
+          return
+        }
+        return
+      }
+      case RESOLVED: {
+        if (draft.status === 'pending' || draft.status === 'updating' || draft.status === 'authorized') {
+          draft.data = action.payload
+          draft.status = 'resolved'
+        }
+        return
+      }
+      case AUTHORIZED: {
+        if (draft.status === 'pending' || draft.status === 'updating') {
+          draft.token = action.payload
+          draft.status = 'authorized'
+        }
+        return
+      }
+      case REJECTED: {
+        if (draft.status === 'pending' || draft.status === 'updating') {
+          draft.error = action.payload
+          draft.data = null
+          draft.status = 'rejected'
+        }
+        return
+      }
+      case RESET: {
+        return
+      }
+      default:
+        return
+    }
+  })
+}
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,59 +1,11 @@
-import { createStore } from 'redux'
-import produce from 'immer'
+import { combineReducers, createStore } from 'redux'
+import userReducer from '../features/user'
 
-// state
-const initialState = {
-    username:null,
-    userlogin: null,
-    password: null,
-    rememberMe: false
-  }
- 
-// actions creators
-export const loginUser = (userlogin, password, rememberMe, username) => ({
-  type: 'login',
-  payload: {
-    userlogin: userlogin,
-    password: password,
-    rememberMe: rememberMe,
-    username: username
-  }
+
+const reducer = combineReducers({
+  user: userReducer,
 })
-
-export const modifyUser = (username, userlogin, password) => ({
-  type: 'modify',
-  payload: {
-    username: username,
-    userlogin: userlogin,
-    password: password,
-  }
-})
-
-export const reset = () => ({ type: 'reset' })
-  
-function reducer(state = initialState, action) {
-  if (action.type === 'login') {
-    return produce(state, draft => {
-      draft.userlogin = action.payload.userlogin
-      draft.password = action.payload.password
-      draft.rememberMe = action.payload.rememberMe
-      draft.username = action.payload.username
-    })
-  }
-  if (action.type === 'modify') {
-    return produce(state, draft => {
-      draft.username = action.payload.username
-      draft.userlogin = action.payload.userlogin
-      draft.password = action.payload.password
-    })
-  }
-  if (action.type === 'reset') {
-    return initialState
-  }
-  return state
-}
 
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-export const store = createStore(reducer, reduxDevTools)
 
-store.subscribe(() => console.log(store.getState()))
+export const store = createStore(reducer, reduxDevTools)
