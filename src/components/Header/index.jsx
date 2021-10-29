08e9efed9868935ec67abb1f93b8b998f965bb3f
David Weiland <weiland.david@icloud.com>
2021-10-29 13:37:28 +0200

    conditional views

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,47 +1,80 @@
 import '../../utils/Style/main.css'
 import Logo from '../../assets/Images/argentBankLogo.png'
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux'
-import {reset} from '../../utils/store'
+import { Link, useLocation } from 'react-router-dom';
+//import { useStore } from 'react-redux'
+import {resetUser} from '../../features/user'
 
 
 function Header() {
-  
-  const username = useSelector((state) => state.username)
-  const dispatch = useDispatch()
+  const location = useLocation()
+  //const store = useStore()
 
-  return (
-    <nav className="main-nav">
-      <Link className="main-nav-logo" to="/">
-        <img
-          className="main-nav-logo-image"
-          src={Logo}
-          alt="Argent Bank Logo"
-        />
-        <h1 className="sr-only">Argent Bank</h1>
-      </Link>
-      {(!username) ? (
-        <div>
-        <Link className="main-nav-item" to="/login">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-        </Link>
-      </div>
-      ) : (
-        <div>
-        <Link className="main-nav-item" to="/profile">
-          <i className="fa fa-user-circle"></i>
-          {username.split(' ')[0]}
-        </Link>
-        <Link className="main-nav-item" onClick={() => dispatch(reset())} to="/">
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-        </Link>
-      </div>
+  //const firstName = store.getState().user.data.firstName
+  const firstName = 'coucou'
+  
+  switch(location.pathname) {
+    case '/login' :
+      return (
+        <nav className="main-nav">
+          <Link className="main-nav-logo" to="/">
+            <img
+              className="main-nav-logo-image"
+              src={Logo}
+              alt="Argent Bank Logo"
+            />
+            <h1 className="sr-only">Argent Bank</h1>
+          </Link>
+          <div>
+            <Link className="main-nav-item" to="/newUser">
+                <i className="fa fa-user-circle"></i>
+                Sign Up
+            </Link>
+          </div>
+        </nav>
       )
-      }
-    </nav>
-  );
+    case '/user' :
+      return (
+        <nav className="main-nav">
+          <Link className="main-nav-logo" to="/">
+            <img
+              className="main-nav-logo-image"
+              src={Logo}
+              alt="Argent Bank Logo"
+            />
+            <h1 className="sr-only">Argent Bank</h1>
+          </Link>
+          <div>
+            <Link className="main-nav-item" to="/profile">
+              <i className="fa fa-user-circle"></i>
+              {firstName}
+            </Link>
+            <Link className="main-nav-item" onClick={() => resetUser()} to="/">
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </div>
+        </nav>
+      )
+    default:
+      return (
+        <nav className="main-nav">
+          <Link className="main-nav-logo" to="/">
+            <img
+              className="main-nav-logo-image"
+              src={Logo}
+              alt="Argent Bank Logo"
+            />
+            <h1 className="sr-only">Argent Bank</h1>
+          </Link>
+          <div>
+            <Link className="main-nav-item" to="/login">
+                <i className="fa fa-user-circle"></i>
+                Sign In
+            </Link>
+          </div>
+        </nav>
+      )
+  }
 }
 
 export default Header;
